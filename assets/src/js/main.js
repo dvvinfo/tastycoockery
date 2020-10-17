5fb6572381bf53dd88bf43dc2ce6ed1041f60ab1
Vladimir Drobyshev <dvvinfo@mail.ru>
2020-10-17 21:50:40 +0300

    Сверстал декстопную версию

---
+++ assets/src/js/main.js
@@ -0,0 +1,7 @@
+// Импортируем jQuery
+//= ../../../node_modules/jquery/dist/jquery.js
+
+
+// Импортируем другие js-файлы
+//= jquery.matchHeight-min.js
+//= my.js
---
+++ assets/src/js/my.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.item').matchHeight(
+        {
+            byRow: true,
+            property: 'height',
+            target: null,
+            remove: false
+        }
+    );
+});
