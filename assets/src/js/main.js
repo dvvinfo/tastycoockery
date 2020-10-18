b6abe33107eaabb51129cf3b1c270c968bdc1e0e
Vladimir Drobyshev <dvvinfo@mail.ru>
2020-10-18 17:18:35 +0300

    Готовая верска

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -1,7 +1,9 @@
 // Импортируем jQuery
 //= ../../../node_modules/jquery/dist/jquery.js
+//= ../../../node_modules/slick-carousel/slick/slick.min.js
 
 
 // Импортируем другие js-файлы
 //= jquery.matchHeight-min.js
+//= jquery.validate.min.js
 //= my.js
--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -7,4 +7,78 @@ $(function() {
             remove: false
         }
     );
+    $('.icons-carousel').slick({
+
+        responsive: [
+            {
+                breakpoint: 100000,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 992,
+                settings:
+                    {
+                        arrows: false,
+                        centerMode: true,
+                        slidesToShow: 1,
+                        dots: true
+                    }
+            },
+
+        ]
+    });
+    $('.about-carousel').slick({
+
+        responsive: [
+            {
+                breakpoint: 100000,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 992,
+                settings:
+                    {
+                        arrows: false,
+                        centerMode: true,
+                        slidesToShow: 1,
+                        dots: true
+                    }
+            },
+
+        ]
+    });
+    $('.reviews-carousel').slick({
+
+        responsive: [
+            {
+                breakpoint: 100000,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 992,
+                settings:
+                    {
+                        arrows: false,
+                        centerMode: true,
+                        slidesToShow: 1,
+                        dots: true
+                    }
+            },
+
+        ]
+    });
+    //валидация формы
+    $("#form").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            },
+
+        },
+
+        submitHandler: function() {
+            alert("форма успешно отпралена");
+        }
+    });
 });
